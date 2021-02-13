fa6d6a43a512a2b2741e218c14cda88eaf911adf
omegazitro <morti137@stu.southtexascollege.edu>
2021-02-12 20:34:56 -0600

    Test

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex flex-col w-full container">
+    <Title msg="🎲 My Bucket List 🎲" />
+    <NewTodo />
+  </header>
+</template>
+
+<script setup>
+import Title from './Title.vue'
+import NewTodo from './NewTodo.vue'
+</script>
